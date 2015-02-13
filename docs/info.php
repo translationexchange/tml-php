d03571342dc338a202fc3363e0de92724820ed7f
Michael Berkovich <theiceberk@gmail.com>
2015-02-12 16:39:30 -0800

    updated cache

--- docs/info.php
+++ docs/info.php
@@ -1,1 +1,11 @@
 <?php phpinfo() ?>
+
+<?php tre("You have [bold: {count || message}]", array("count" => 1, "bold" => '<a href="http://www.google.com">{$0}</a>')) ?>
+
+<br><br>
+
+<?php tre("You have {count || message}", array("count" => 2)) ?>
+
+<br><br>
+
+<?php tre("You have {count || message}", array("count" => 5)) ?>
--- docs/test.php
+++ docs/test.php
@@ -2,21 +2,8 @@
 
 <div class="container">
 
-<?php tml_begin_block_with_options(array("source" => "/test")) ?>
+<?php tre("Hello {user}", array("user" => "Michael")) ?>
 
-<br>
-
-<?php tre("You have [bold: {count || message}]", array("count" => 1, "bold" => '<a href="http://www.google.com">{$0}</a>')) ?>
-
-<br><br>
-
-<?php tre("You have {count || message}", array("count" => 2)) ?>
-
-<br><br>
-
-<?php tre("You have {count || message}", array("count" => 5)) ?>
-
-<?php tml_finish_block_with_options() ?>
 
 </div>
 
