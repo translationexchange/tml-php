51f71a372640aaea4e0b8c30d74d7ce2e3ef13d9
Michael Berkovich <michael@translationexchange.com>
2016-05-06 11:14:00 -0700

    added test

--- public/test.php
+++ public/test.php
@@ -2,8 +2,9 @@
 
 <div class="container">
 
-<?php tre("Hello {user}", array("user" => "Michael")) ?>
+<?php $user = array("gender" => "female", "name" => "Michael") ?>
 
+<?php tre("<link>{user}</link> tagged {user | himself, herself} in <bold>{count || photo}</bold>.", array("user" => array($user, $user["name"]), "count" => 1, "link" => array("href" => "http://www.yahoo.com"), "bold" => '<span style="color: red">{$0}</span>')) ?>
 
 </div>
 
