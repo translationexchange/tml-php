9ef5087e4336af74aaee854a454a00d965e34793
Michael Berkovich <michael@translationexchange.com>
2016-11-24 14:09:40 -0800

    support for php 5.3

--- public/www/tml/_index.php
+++ public/www/tml/_index.php
@@ -1,6 +1,6 @@
 <h1><?php tre("Translation Markup Language") ?></h1>
 
-<?= trh("
+<?php echo trh("
 <p>
     Translation Markup Language (TML) is used to identify the non-translatable or dynamic data within the labels. It provides a way to mark data and decoration tokens within the strings that need to be translated. There are different types of applications that can use TML - web, mobile and desktop. Some use HTML, others use Wiki-Like syntax for decorating the labels. TML aims at abstracting out the decoration mechanisms of the string used by the applications and instead provides its own simple, but powerful syntax. This allows for translation sharing across multiple applications.
 </p>
